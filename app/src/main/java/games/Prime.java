963c51045108ae6e053e0aace098aaa84b666001
Dracula <ammaddeuss@gmail.com>
2023-02-23 16:36:45 +0400

    updated class Prime

--- app/src/main/java/games/Prime.java
+++ app/src/main/java/games/Prime.java
@@ -1,6 +1,9 @@
 package games;
 
 import hexlet.code.Cli;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Prime {
@@ -14,11 +17,11 @@ public class Prime {
 
         while (count < 3) {
             Scanner answer = new Scanner(System.in);
-            String primeNumbers =  "2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, "
-                    + "59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, "
-                    + "151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199";
+            List<Integer> primeNumbers = new ArrayList<>(List.of(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
+                    59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149,
+                    151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199));
 
-            String question = (int) (Math.random() * 199) + "";
+            int question = (int) (Math.random() * 199);
             boolean checkAnswer = primeNumbers.contains(question);
 
 
@@ -26,7 +29,7 @@ public class Prime {
             String answerNext = answer.next();
             System.out.println("Your answer: " + answerNext);
 
-            if (!(checkAnswer) || question.equals("1")) {
+            if (!(checkAnswer)) {
                 currentAnswer = "no";
             }
 
