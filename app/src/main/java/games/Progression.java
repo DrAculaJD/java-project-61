a49ae22bb4b5aaffe342b554d5f5eb9cd0933a55
Dracula <ammaddeuss@gmail.com>
2023-02-23 14:10:02 +0400

    added class Progression in package "games"

--- app/src/main/java/games/Progression.java
+++ app/src/main/java/games/Progression.java
@@ -1,4 +1,49 @@
 package games;
 
+import hexlet.code.Cli;
+import java.util.Scanner;
+
 public class Progression {
+
+    public static void game() {
+        Cli.greetings();
+        System.out.println("What number is missing in the progression?");
+
+        int count = 0;
+
+        while (count < 3) {
+            Scanner answer = new Scanner(System.in);
+            String[] progression = new String[10];
+            progression[0] = (int) (Math.random() * 100) + "";
+
+            int delta = (int) (Math.random() * 10); // элемент с таким же индексом буду ислючать из прогрессии
+
+            for (int i = 1; i < progression.length; i++) {
+                progression[i] = Integer.parseInt(progression[i - 1]) + delta + "";
+            }
+            String answerNum = progression[delta];
+            progression[delta] = "..";
+
+
+            System.out.println("Question: " + String.join(" ", progression));
+            String answerNext = answer.next();
+            System.out.println("Your answer: " + answerNext);
+
+            if (answerNext.equals(answerNum)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + answerNext + "' is wrong answer ;(. Correct answer was '" + answerNum
+                        + ".\nLet's try again, " + Cli.getName() + "!");
+                break;
+            }
+
+            count++;
+        }
+
+        if (count == 3) {
+            System.out.println("Congratulations, " + Cli.getName() + "!");
+        }
+
+    }
+
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import games.Calc;
 import games.Even;
 import games.GCD;
+import games.Progression;
 
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class App {
                 + "2 - Even\n"
                 + "3 - Calc\n"
                 + "4 - GCD\n"
+                + "5 - Progression\n"
                 + "0 - Exit");
 
         Scanner answer = new Scanner(System.in);
@@ -23,6 +25,7 @@ public class App {
             case "2" -> Even.game();
             case "3" -> Calc.game();
             case "4" -> GCD.game();
+            case "5" -> Progression.game();
             default -> {
             }
         }
